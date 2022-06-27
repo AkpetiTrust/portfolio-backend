befdf978603ab44a7f76671297b8aec1f6857c9b
Akpeti Trust <akpetitrust@gmail.com>
2022-06-27 10:55:13 +0100

    Added new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,9 @@ class DatabaseSeeder extends Seeder
             'email' => "akpetitrust@gmail.com",
             'password' => Hash::make('password'),
         ]);
+        \App\Models\Settings::create([
+            "currently" => "I'm currently learning product design. When I'm not working you can find me watching a movie. Or I'm probably just sleeping 😴 .",
+            "is_available" => false,            
+        ]);
     }
 }
